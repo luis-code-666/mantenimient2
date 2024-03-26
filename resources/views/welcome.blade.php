bd040766a762e4baf0123897454cf7f945002672
rockband_666 <rockband_666@hotmail.com>
2024-03-25 22:58:24 -0400

    mi avamce de esta noche

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,19 @@
         </nav>
 
         <main class="container">
+<div
+    class="row"
+>
+    <div class="col-md-2"></div>
+
+    <div class="col-md-8">
+
+    </div>
+
+    <div class="col-md-2"></div>
+</div>
+
+
 
         </main>
         <!-- Bootstrap JavaScript Libraries -->
